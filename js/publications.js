8053c3afa8517591559bd47e4ed4f4df90d99ec9
Kkrle <levay.kristof@gmail.com>
2021-09-12 15:18:35 +0200

    Fully automatise publication reader script, hide empty years when filtering, make publications titles clickable, style copyrights, place script to the beginning of Publications

--- js/publications.js
+++ js/publications.js
@@ -1,4 +1,11 @@
 function show(query){
+    //show all years
+    var years = document.querySelectorAll(".tudelft-year");
+    for (let index = 0; index < years.length; index++) {
+        const element = years[index];
+        element.style.display = "block";
+    }
+
     var publications = document.querySelectorAll(".tudelft-publication");
     if (query) query = "-"+query;
     for (let index = 0; index < publications.length; index++) {
@@ -10,6 +17,21 @@ function show(query){
         else{
             element.style.display="none";
         }
+    }
+
+    //hide years in which all publications are hidden
+    for (let index = 0; index < years.length; index++) {
+        const element = years[index];
+        var publicationsinYear = element.querySelectorAll(".tudelft-publication");
+        var empty = true;
+        for (let index = 0; index < publicationsinYear.length; index++) {
+            const onePublication = publicationsinYear[index];
+            if (onePublication.style.display!="none") {
+                empty = false;
+                break;
+            }
+        }
+        if (empty) element.style.display="none";
     }
 }
 function filter(category){
